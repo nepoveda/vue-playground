9d65077751dc7a9970e224dbc251f1fe54accd92
Dan Nepejchal <dan.nepejchal@dhl.com>
2026-01-29 15:09:48 +0100

    Basic styling and task

--- todo-app/src/components/TaskList.vue
+++ todo-app/src/components/TaskList.vue
@@ -1,6 +1,6 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 import {useTaskStore} from "@/stores/task.ts";
-import {FwbHeading} from "flowbite-vue";
+import {FwbCheckbox, FwbHeading} from "flowbite-vue";
 
 const tasksStore = useTaskStore()
 
@@ -8,12 +8,26 @@ const tasksStore = useTaskStore()
 
 <template>
   <div class="w-4/5 m-auto">
-    <fwb-heading tag="h2">Task List</fwb-heading>
-    <fwb-heading tag="h2" v-for="task in tasksStore.tasks">Store: {{ task.name }}</fwb-heading>
+    <fwb-heading class="mb-2 p-2" tag="h2">Task List</fwb-heading>
+    <div v-for="task in tasksStore.tasks" class="m-4 p-3 border-2 border-white">
+      <fwb-heading tag="h2">{{ task.name }}</fwb-heading>
+      <p>{{ task.detail }}</p>
+      <fwb-checkbox v-model="task.isDone" disabled/>
+
+    </div>
   </div>
 
 </template>
 
 <style scoped>
+h2 {
+  font-size: 1.5rem;
+  font-weight: bold;
+  text-decoration: underline;
+}
+
+p {
+  white-space: pre-line;
+}
 
 </style>
